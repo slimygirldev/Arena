82f24779b71f364f7676cf933968ca4ee2b1f5e4
Lorene Brocourt <lorenebrocourt@gmail.com>
2022-05-16 15:39:55 +0200

    Add choseName func for Player
    fixes #2

--- Arena/Game.swift
+++ Arena/Game.swift
@@ -6,10 +6,56 @@
 //
 
 import Foundation
+/*
+ Game has :
+ init (Player1: Player, Player2: Player)
+ Players
+ function start() to launch the party
+
+ */
 
 class Game {
+
+    var playerOne: Player
+    var playerTwo: Player
+
+    init() {
+
+        var player1: Player = Player()
+        playerOne = player1
+        var player2: Player = Player()
+        playerTwo = player2
+
+    }
+
+
     func start() {
         print("Game start")
+        welcome()
+        initPlayers()
+
+    }
+
+    func initPlayers() {
+        playerOne = Player()
+        playerOne.choseName()
+        playerTwo = Player()
+        playerTwo.choseName()
+    }
+
+
+    func welcome() {
+        print("""
+Welcome to the Arena !!
+The only place where you can test you abilities to combat !
+Please join the next tournament by enter your name ! \n
+""")
+    }
+    
+    func fight() {
+
     }
 }
+
+
 
--- Arena/Guild.swift
+++ Arena/Guild.swift
@@ -7,21 +7,31 @@
 
 import Foundation
 /*
+ is the Factory of the chracters
  creat a sort of fighter
  ex : creatWarrior()
  call one of the fighter class
  */
+
+// MARK: - creat a wwarrior
+
 func callWarrior() {
 
 }
+
+// MARK: - creat a Magus
 
 func callMagus() {
 
 }
 
+// MARK: - creat a Colossus
+
 func callColossus() {
 
 }
+
+// MARK: - creat a Dwark
 
 func callDwarf() {
 
